fa3359000e5b347d63f03f1cb8ab2cced1a0ee5e
Henrik N <henrik@nyh.se>
2008-10-23 13:31:14 +0200

    Uncomment code that breaks Rails <2.1 under some conditions, but that may be necessary with STI.
    Not a pleasant solution, but better than nothing for now.

--- init.rb
+++ init.rb
@@ -1,9 +1,13 @@
 require 'thinking_sphinx'
 require 'action_controller/dispatcher'
 
-if Rails::VERSION::STRING.to_f < 2.1
-  ThinkingSphinx::Configuration.instance.load_models
-end
+# With Rails 2.1, you may need to uncomment this if your indexed models use STI, but
+# otherwise you risk breakage, e.g if you have observers watching an indexed model.
+# See http://groups.google.com/group/thinking-sphinx/browse_thread/thread/9f29b59f6175f50f
+
+# if Rails::VERSION::STRING.to_f < 2.1
+#   ThinkingSphinx::Configuration.instance.load_models
+# end
 
 ActionController::Dispatcher.to_prepare :thinking_sphinx do
   ThinkingSphinx::Configuration.instance.load_models
